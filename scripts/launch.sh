f03555fb5d04a6dbb4ea922561ec52aef95942b3
BAkhtiyar <cena61454@gmail.com>
2023-09-24 19:11:42 +0600

    learn_planr v0.1.4

--- scripts/launch.sh
+++
@@ -1,6 +0,0 @@
-export DJANGO_SETTINGS_MODULE=learn_planr.settings
-python learn_planr/manage.py collectstatic --noinput
-echo 'Applying migrations...'
-python learn_planr/manage.py migrate
-
-gunicorn learn_planr.learn_planr.wsgi:application --bind 0.0.0.0:$PORT
